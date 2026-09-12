a389f20ba82db3f228dcb120e2eed0167fe4cd4b
Mateo Rettenberger <mateo@example.com>
2026-09-12 21:39:08 +0200

    Remove temporary test controller and route

--- backend/routes/test.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const testController = require('../controllers/testController');
-
-router.delete('/raw-delete/:id', testController.rawDeleteAndCheck);
-
-module.exports = router;
